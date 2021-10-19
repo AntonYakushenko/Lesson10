95c5982f5306c7b974c1593ad5879b5d8c129496
AntonYakushenko <mnbv0@rambler.ru>
2021-10-20 02:28:02 +0400

    1

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -20,7 +20,7 @@ public class TestBase {
 
         Configuration.browserCapabilities = capabilities;
         Configuration.startMaximized = true;
-       Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+        //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
     }
 
     @AfterEach
