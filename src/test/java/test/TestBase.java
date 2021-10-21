fd23e3e82edb6433b2a86ebd11ad64ecdc0ca826
Toni773 <75640559+AntonYakushenko@users.noreply.github.com>
2021-10-22 03:40:07 +0400

    Update TestBase.java

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -21,7 +21,8 @@ public class TestBase {
         Configuration.browserCapabilities = capabilities;
         Configuration.startMaximized = true;
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+        //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+        Configuration.remote = "http://137.184.95.234:8080/";
     }
 
     @AfterEach
