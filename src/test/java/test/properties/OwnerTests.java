184557815e9b91620950e6e63fde09e3d26a2b76
antonyakusenko <mnbv0@rambler.ru>
2021-10-28 18:52:01 +0400

    add creds

--- src/test/java/test/properties/OwnerTests.java
+++ src/test/java/test/properties/OwnerTests.java
@@ -5,11 +5,14 @@ import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static java.lang.String.format;
+
+@Tag("properties")
 public class OwnerTests {
     public  CredentialsConfig credentials =
             ConfigFactory.create(CredentialsConfig.class);
 
-    @Tag("properties")
+
     @Test
     void readCredentialsTest() {
         String login = credentials.login();
@@ -17,6 +20,9 @@ public class OwnerTests {
 
         System.out.println(login);
         System.out.println(password);
+
+        String message = format("i login as %s with password %s", login, password);
+        System.out.println(message);
     }
 
 }
